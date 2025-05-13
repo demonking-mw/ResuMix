e5d7d2e8dee119764d8cec5d2cff08db73b8a803
demonking-mw <m574wang@uwaterloo.ca>
2025-05-13 15:12:58 -0400

    setup mostly complete, ready for user implementation

---
+++ backend/src/classes_req/user_req.py
@@ -0,0 +1,35 @@
+"""
+input requirments for user class
+
+Requirement naming rule:
+lowercase_with_underscore
+
+Commenting: ALL_CAP for required, lowercase for optional
+"""
+
+from flask_restful import reqparse
+
+# User Auth: TYPE, jwt_token, reauth_jwt, action, email, uid, pwd, user_name
+#################################################################
+user_auth = reqparse.RequestParser()
+user_auth.add_argument(
+    "type",
+    type=str,
+    help="Type is required, can be eup or go or jwt or jwt_check",
+    required=True,
+)
+user_auth.add_argument(
+    "jwt_token", type=str, help="JWT token for go type", required=False, default=None
+)
+user_auth.add_argument(
+    "reauth_jwt",
+    type=str,
+    help="JWT token provided from first login, good for 1h",
+    required=False,
+    default=None,
+)
+user_auth.add_argument("action", type=str, help="login/signup", required=False)
+user_auth.add_argument("email", type=str, help="Email", required=False)
+user_auth.add_argument("uid", type=str, help="User ID", required=False)
+user_auth.add_argument("pwd", type=str, help="Password", required=False)
+user_auth.add_argument("user_name", type=str, help="Name", required=False)
