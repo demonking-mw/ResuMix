5924fc7e3f1f2a966b94ace7739976dee2599d1c
demonking-mw <m574wang@uwaterloo.ca>
2025-08-11 15:14:04 -0400

    setup updated

--- backend/src/segment_tests/opt_test2.py
+++ backend/src/segment_tests/opt_test2.py
@@ -2,8 +2,8 @@ from sentence_transformers import SentenceTransformer
 import os
 
 model_path = "all-mpnet-base-v2-local"
-if not os.path.exists(model_path):
-    # 1) download the model once
-    model = SentenceTransformer("all-MiniLM-L12-v2")
-    # 2) save it to a folder
-    model.save(model_path)
+# 1) download the model once
+model = SentenceTransformer("all-MiniLM-L12-v2")
+# 2) save it to a folder
+model.save(model_path)
+print(f"Model saved to {model_path}")
