2300feb0d459c8b61a0bf23de2bcde1e236df0e8
demonking-mw <m574wang@uwaterloo.ca>
2025-08-11 15:41:59 -0400

    faster model

--- backend/src/segment_tests/opt_test2.py
+++ backend/src/segment_tests/opt_test2.py
@@ -3,7 +3,7 @@ import os
 
 model_path = "all-mpnet-base-v2-local"
 # 1) download the model once
-model = SentenceTransformer("all-MiniLM-L12-v2")
+model = SentenceTransformer("paraphrase-MiniLM-L3-v2")
 # 2) save it to a folder
 model.save(model_path)
 print(f"Model saved to {model_path}")
